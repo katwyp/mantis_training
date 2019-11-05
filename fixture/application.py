3a039eb62e2e97da515f08f73421df40ffbab042
katarzyna <katarzyna_wypich@parp.gov.pl>
2019-11-05 14:02:47 +0100

    Pierwszy test

---
+++ fixture/application.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+
+class Application:
+
+    def __init__(self, browser, base_url):
+        if browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "edge":
+            self.wd = webdriver.Edge()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.session = SessionHelper(self)
+        self.base_url = base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+        if not (wd.current_url.endswith == "/my_view_page.php"):
+            wd.get(self.base_url)
+
+    def destroy(self):
+        self.wd.quit()
